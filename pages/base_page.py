73bd232936a12bbabf549f6c5609e6ec77800de7
BlavikensButcher1989 <stupid6_stupid@mail.ru>
2025-05-11 14:49:55 +0300

    update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,7 +1,6 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait as W
 from selenium.webdriver.common.action_chains import ActionChains
-from selenium.common.exceptions import TimeoutException
 
 class BasePage:
     def __init__(self, driver, url):
