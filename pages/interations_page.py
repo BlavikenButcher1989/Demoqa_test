8d6d21eca1aef97ed639f37e362385160a1cda90
BlavikensButcher1989 <stupid6_stupid@mail.ru>
2025-06-14 16:26:48 +0300

    added class: - TestSelectablePage added test: - test_selectable_list - test_selectable_grid

--- pages/interations_page.py
+++ pages/interations_page.py
@@ -3,6 +3,7 @@ import time
 
 from pages.base_page import BasePage
 from locators.interactions_locators import SortablePageLocators
+from locators.interactions_locators import SelectablePageLocators
 
 class SortablePage(BasePage):
 
@@ -37,3 +38,34 @@ class SortablePage(BasePage):
         order_after = self.get_sortable_items(self.locators.GRID_ITEMS)
 
         return order_before, order_after
+
+class SelectablePage(BasePage):
+
+    def __init__(self, driver, url):
+        super().__init__(driver, url)
+
+    locators = SelectablePageLocators()
+
+    def click_elements(self, elements):
+        count_items = 0
+        items_list = random.sample(self.elements_are_visible(elements),
+                                   random.randint(1, len(self.elements_are_visible(elements))))
+        for item in items_list:
+            item.click()
+            count_items += 1
+
+        return count_items
+
+    def change_list_order(self):
+        self.element_is_visible(self.locators.TAB_LIST).click()
+        count_item = self.click_elements(self.locators.LIST_ITEM)
+        item_list_active = self.elements_are_visible(self.locators.LIST_ITEM_ACTIVE)
+
+        return count_item, len(item_list_active)
+
+    def change_grid_order(self):
+        self.element_is_visible(self.locators.TAB_GRID).click()
+        count_item = self.click_elements(self.locators.GRID_ITEM)
+        item_grid_active = self.elements_are_visible(self.locators.GRID_ITEM_ACTIVE)
+
+        return count_item, len(item_grid_active)
--- tests/interactions_test.py
+++ tests/interactions_test.py
@@ -1,4 +1,5 @@
 from pages.interations_page import SortablePage
+from pages.interations_page import SelectablePage
 
 
 class TestSortablePage:
@@ -14,3 +15,17 @@ class TestSortablePage:
         sortable_page.open_site()
         order_before, order_after = sortable_page.change_grid_order()
         assert order_before != order_after, 'Order of teh grid has not been changed'
+
+class TestSelectablePage:
+
+    def test_selectable_list(self, driver):
+        selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+        selectable_page.open_site()
+        count_item, item_list_active = selectable_page.change_list_order()
+        assert count_item == item_list_active, 'elements were not selected'
+
+    def test_selectable_grid(self, driver):
+        selectable_page = SelectablePage(driver, 'https://demoqa.com/selectable')
+        selectable_page.open_site()
+        count_item, item_grid_active = selectable_page.change_grid_order()
+        assert count_item == item_grid_active, 'elements were not selected'
